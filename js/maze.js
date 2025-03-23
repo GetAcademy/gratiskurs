d002c9de7c4b832562b7327da643fc8b2b3d072f
GET Academy <terje@getacademy.no>
2025-03-23 09:11:46 +0100

    Update maze.js

--- js/maze.js
+++ js/maze.js
@@ -171,13 +171,13 @@ class MazeCanvas extends HTMLElement {
         let x = this.calcWallSize(colIndex) + model.cornerSize + this.offsetX;
         let y = this.calcWallSize(rowIndex) - model.cornerSize + this.offsetY;
         const size = model.wallSize + model.cornerSize;
-        if (!this.shouldDraw(roomIndex)) {
-            const fillStyle = this.ctx.fillStyle;
-            this.ctx.fillStyle = 'black';
-            this.ctx.fillRect(x, y, size, size);
-            this.ctx.fillStyle = fillStyle;
-            return;
-        }
+        // if (!this.shouldDraw(roomIndex)) {
+        //     const fillStyle = this.ctx.fillStyle;
+        //     this.ctx.fillStyle = 'black';
+        //     this.ctx.fillRect(x, y, size, size);
+        //     this.ctx.fillStyle = fillStyle;
+        //     return;
+        // }
         const isOpenUp = model.openWalls['opp' + roomIndex] || false;
         const isOpenRight = model.openWalls['høyre' + roomIndex] || false;
         const isOpenDown = model.openWalls['ned' + roomIndex] || false;
@@ -212,8 +212,8 @@ class MazeCanvas extends HTMLElement {
             }
         }
 
-        if (isOpenLeft) {
-            const wallX = x - model.cornerSize; // Flytt til venstre
+        if (isOpenRight) {
+            const wallX = x + 64; 
             const wallY = y;
             const wallWidth = model.cornerSize; // Kun de venstre 16 pikslene
             const wallHeight = model.wallSize;
